05626891f653d7ec9a31dbbc0f69f100ca2e05a6
Elvira Menshikova <em97-97@mail.ru>
2023-05-24 23:43:14 +0400

    src

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -13,41 +13,42 @@ import static com.codeborne.selenide.Selenide.*;
 
 
 public class CardDeliveryTest {
-    @Test   // №1
+    @Test
     public void shouldSuccessfulFormSubmission() {
         open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Анадырь");
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
-        String verificationDate = LocalDate.now() .plusDays(3)         //Текущая дата плюс 3 дня
-                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));     //Формат даты день.месяц.год
+        String verificationDate = LocalDate.now().plusDays(3)
+                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id=date] input").setValue(verificationDate);
-        $("[data-test-id=name] input").setValue("Виктор Иванов");
-        $("[data-test-id=phone] input").setValue("+79206116499");
+        $("[data-test-id=name] input").setValue("Иванов Иванов");
+        $("[data-test-id=phone] input").setValue("+71234567890");
         $("[data-test-id=agreement]").click();
         $x("//*[text()=\"Забронировать\"]").click();
         $("[data-test-id=notification]")
                 .shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + verificationDate),
-                        Duration.ofSeconds(15));                        //Загрузка не более 15 секунд
+                        Duration.ofSeconds(15));
     }
+
     @Test   //№2
     public void shouldSuccessfulFormSubmissionAfterInteractingWithComplexElements() {
         open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Ан");
         $(Selectors.byText("Анадырь")).click();
         $("[data-test-id=date] input").click();
-        int days = 4;                                                   //количество дней после даты по умолчанию
-        for ( int cycle = 0; cycle < days; cycle++) {
+        int days = 4;
+        for (int cycle = 0; cycle < days; cycle++) {
             $(".calendar").sendKeys(Keys.ARROW_RIGHT);
         }
         $(".calendar").sendKeys(Keys.ENTER);
-        $("[data-test-id=name] input").setValue("Виктор Иванов");
-        $("[data-test-id=phone] input").setValue("+79206116499");
+        $("[data-test-id=name] input").setValue("Иван Иванов");
+        $("[data-test-id=phone] input").setValue("+71234567890");
         $("[data-test-id=agreement]").click();
         $(".button").shouldHave(Condition.text("Забронировать")).click();
-        String verificationDate = LocalDate.now().plusDays(7)           //Текущая дата плюс 7 дней
-                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));     //Формат даты день.месяц.год
+        String verificationDate = LocalDate.now().plusDays(7)
+                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id=notification]")
                 .shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + verificationDate),
-                        Duration.ofSeconds(15));                        //Загрузка не более 15 секунд
+                        Duration.ofSeconds(15));
     }
 }
